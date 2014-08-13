5b71dd0d02a8cf9359a35d3560bd563aedbd2df4
Thomas Grice <tgrice@esi-estech.com>
2014-08-13 11:16:41 -0500

    delete stuff, get cardsets menu to work
    delete stuff, get cardsets menu to work

--- public/modules/decks/controllers/decks.client.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('cards').controller('CardsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Cards',
-	function($scope, $stateParams, $location, Authentication, Cards) {
-		$scope.authentication = Authentication;
-	}
-]);
